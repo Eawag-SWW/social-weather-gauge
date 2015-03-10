f48da6636960259262f1b860a18acc242d504612
Dominic Looser <dominic.looser@gmail.com>
2015-03-10 21:24:32 +0100

    misc

--- api.py
+++ api.py
@@ -1,12 +1,22 @@
 import flickrapi
 
 import secrets
+import constants
 
 FORMAT = 'parsed-json'
 API_KEY = secrets.API_KEY
 API_SECRET = secrets.API_SECRET
 
-api = flickrapi.FlickrAPI(API_KEY, API_SECRET, format=FORMAT)
+flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET, format=FORMAT)
+
+
+class Photo:
+    def __init__(self, data):
+        self._data = data
+
+    def get_date(self):
+        pass
+
 
 class Photos:
 
@@ -25,10 +35,27 @@ class Photos:
             self._current += 1
             return self._data[self._current - 1]
 
+    def get_size(self):
+        return len(self._data)
+
+
+def get_params(params_name):
+    if params_name == 'switzerland':
+        switzerland_params = dict()
+        switzerland_params['woe_id'] = constants.WOE_ID_SWITZERLAND
+        return switzerland_params
+    else:
+        raise Exception('invalid params name')
+        # thun_params = dict()
+        # thun_params['tags'] = tags
+        # thun_params['lat'] = 47.566667 # 46.76
+        # thun_params['lon'] =  7.6 # 7.624
+        # thun_params['radius'] = RADIUS
+        # params_list.append(thun_params)
+
 
 def get_photos(params):
-
-    response = api.photos.search(**params)
+    response = flickr.photos.search(**params)
     photos = Photos(response['photos']['photo'])
     photos.total = int(response['photos']['total'])
     return photos
--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,21 @@
-
-from api import api
-import constants
+import api
+from api import flickr
 
 
 def tags_for_place(woe_id):
-    response = api.places.tagsForPlace(woe_id=woe_id)
+    response = flickr.places.tagsForPlace(woe_id=woe_id)
     print(response)
+
 
 def find_place(query):
-    response = api.places.find(query=query)
+    response = flickr.places.find(query=query)
     print(response)
 
 
+def sandbox():
+    params = api.get_params('switzerland')
+    photos = api.get_photos(params) 
+    print photos.get_size()
 
-find_place('Schweiz')
+
+sandbox()
