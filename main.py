a7e605c91f5bd1385f4b51943652f725b1967820
Dominic Looser <dominic.looser@gmail.com>
2015-03-10 21:24:32 +0100

    misc

--- main.py
+++ main.py
@@ -4,36 +4,23 @@
 import random
 
 import matplotlib.pyplot as plotter
-import flickrapi
-
 import api
 import constants
-tags_list = [['hochwasser'], ['überschwemmung'], ['überflutung'], ['flut']]
 
+tags_list = [['hochwasser'], ['überschwemmung'], ['überflutung'], ['flut']]
 RADIUS = 30
+
 
 class DataSet:
     pass
 
 
 
-def get_params(params_name):
-    if params_name == 'switzerland':
-        switzerland_params = dict()
-        switzerland_params['woe_id'] = constants.WOE_ID_SWITZERLAND
-        return switzerland_params
-    else:
-        raise Exception('invalid params name')
-        # thun_params = dict()
-        # thun_params['tags'] = tags
-        # thun_params['lat'] = 47.566667 # 46.76
-        # thun_params['lon'] =  7.6 # 7.624
-        # thun_params['radius'] = RADIUS
-        # params_list.append(thun_params)
 
 
         params['switzerland'] = switzerland_params
     return get_params
+
 
 def print_random_links(response):
     photos = response['photos']['photo']
@@ -41,6 +28,7 @@ def print_random_links(response):
         photo = random.choice(photos)
         url = 'https://www.flickr.com/photos/%s/%s' % (photo['owner'], photo['id'])
         print(url)
+
 
 def get_data_set():
     params = get_params('switzerland')
@@ -53,6 +41,7 @@ def get_data_set():
     return DataSet()
     # for photo in photos:
     #     date = photo.get_date()
+
 
 def plot_switzerland():
     data_set = get_data_set()
