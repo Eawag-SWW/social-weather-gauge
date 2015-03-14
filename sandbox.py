7c1ae8e860f2834f92b2bc23c8d3369987f01e12
Dominic Looser <dominic.looser@gmail.com>
2015-03-14 18:56:45 +0100

    misc

--- sandbox.py
+++ sandbox.py
@@ -1,10 +1,11 @@
 import numpy
 from matplotlib import pyplot as plotter
-import facebook
+from pattern.web import Twitter
 
 import api
 from api import Query
 from api import flickr
+import secrets
 
 
 def tags_for_place(woe_id):
@@ -22,6 +23,7 @@ def sandbox():
     photos = api.get_photos(params) 
     print photos.get_size()
 
+
 def walking():
     params = api.get_params('switzerland')
     params['min_upload_date'] = '2015-01-01'
@@ -29,20 +31,27 @@ def walking():
     photos = api.get_photos(params)
     print photos.total
 
+
 def array():
-    x = [1,2,3]
-    y = [1,4,9]
-    data = numpy.array([x,y])
+    x = [1, 2, 3]
+    y = [1, 4, 9]
+    data = numpy.array([x, y])
     numpy.savetxt('test.csv', data)
     loaded = numpy.loadtxt('test.csv')
     x = loaded[0]
     y = loaded[1]
-    plotter.plot(x,y)
+    plotter.plot(x, y)
     plotter.show()
     print data
 
+
 def facebook():
-    graph = facebook.GraphAPI()
+    api = Twitter()
 
+    results = api.search('Hochwasser')
+    print len(results)
+    for result in results:
+        print dir(result)
+        break
 
-print Query.switzerland_flooding.name
+facebook()
